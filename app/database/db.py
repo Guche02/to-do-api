01a4a9b31e1a9acd235a70d3701659c0721db9de
Guche02 <loozasubedy998@gmail.com>
2025-01-22 14:13:29 +0545

    Dockerized fastapi and mySQL database

--- app/database/db.py
+++ app/database/db.py
@@ -1,13 +1,14 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-load_dotenv()
-import os
-DB_URL = DB_URL = os.getenv("DB_URL")
-engine = create_engine(DB_URL,echo=True)
-SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
+# Make sure the .env file in the `app` folder is loaded
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+DB_URL = os.getenv("DB_URL")  # Assuming DB_URL is stored in the .env file
+engine = create_engine(DB_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
